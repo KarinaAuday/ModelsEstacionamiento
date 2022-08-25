4465ef320254d15dc8941b6d94219cf8261770eb
KARINA AUDAY <karina.auday@ort.edu.ar>
2022-08-25 12:09:52 -0300

    Cree la carpeta Models , y alli puse las clases con las que estamos trabajando. tambien se muestra por pantalla los valores

--- Models/Program.cs
+++ Models/Program.cs
@@ -10,7 +10,9 @@ namespace ClasesEstacionamiento
             Persona persona = new Persona("Jose","gonzalez");
             persona.Id = 1;
             Persona persona1 = new Persona() { Apellido = "Perez", Nombre = "Juan", Dni = 123456 };
-
+            Console.WriteLine(String.Format("{0},{1},{2}", persona1.Nombre , persona1.Apellido , persona1.Dni));
+            Console.WriteLine(String.Format("{0},{1},{2}", persona.Nombre, persona.Apellido, persona.Dni));
+            Console.ReadKey();
         }
     }
 }
